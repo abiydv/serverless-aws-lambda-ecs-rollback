0607139941916dc053630b0be8ca6026feda840d
abiydv <32677073+abiydv@users.noreply.github.com>
2019-02-02 00:36:55 +0530

    Serverless initial (#1)

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
-# serverless-aws-lambda-ecs-rollback
-AWS Lambda to rollback ECS deployment. Created with serverless framework and nodejs
+# Serverless: AWS Lambda to rollback AWS ECS deployment
+
+Serverless framework example to deploy AWS Lambda, which can rollback a ECS deployment to the second last 'active' task definition version. Lambda runtime used - nodejs 8.x
+
+## Prerequisites
+   **1.** Install serverless. Follow this [guide](https://serverless.com/framework/docs/providers/aws/guide/installation/)
+   
+   **2.** Setup aws cli with profiles matching environments/stages. A sample `~/.aws/credentials` file - 
+   
+   ```
+   [dev]
+   aws_access_key_id = DEV_ACCESS_KEY
+   aws_secret_access_key = DEV_SECRET_KEY
+   [qa]
+   aws_access_key_id = QA_ACCESS_KEY
+   aws_secret_access_key = QA_SECRET_KEY
+   [prod]
+   aws_access_key_id = PROD_ACCESS_KEY
+   aws_secret_access_key = PROD_SECRET_KEY
+   ```
+
+## How to use
+
+  **1.** Deploy the service with `--stage dev` argument to create the `dev` stack. Replace `dev` with `qa` or `prod` to launch function in other environments. <br><br>
+  ***NOTE**: Create `config-qa.json` and `config-prod.json` with the respective AWS Account Id info.*
+
+  ```
+  serverless deploy -v --stage dev
+  ```
+
+  **2.** Invoke the lambda function
+  ```
+  serverless invoke -f rollback -l --stage dev
+  ```
+
+  **3.** Deploy only the lambda function after any change in lambda code. (Skip if no change)
+  ```
+  serverless deploy -f rollback --stage dev
+  ```
+
+  **4.** Cleanup everything
+  ```
+  serverless remove -v --stage dev
+  ```
+
+## Contact
+
+Drop me a note or open an issue if something doesn't work out.
+
+Cheers! :thumbsup:
---
+++ handler.js
@@ -0,0 +1,87 @@
+'use strict';
+
+const AWS = require('aws-sdk');	
+const ecs = new AWS.ECS({ region: 'us-east-1' });
+
+module.exports.rollback = (event, context) => {
+	
+	const resp = main(process.env.ECS_SERVICE,process.env.ECS_CLUSTER,process.env.ECS_TASK); 
+	return resp;
+
+	async function main(service,cluster,task){
+
+		try {
+			var td = await gettaskdef(service,cluster);
+			console.log("Service task definition: ", td);
+		} catch (error) { 
+			console.log("Unable to get task definition");
+			return error;
+		}
+
+		try { 
+			var rbtd = await listtaskdef(task);
+			if ( td === rbtd ){
+				console.log("Already on the second last version. Nothing to do");
+				return "Already on the second last version. Nothing to do";
+			} else {
+				console.log("On the latest version, will rollback to second last version");
+			}
+		} catch (error) {
+			console.log("Unable to list task definition");
+			return error;
+		}
+		
+		try {
+			console.log("Rollback triggered: ", await updateservice(service,cluster,rbtd));
+		} catch (error){
+			console.log("Unable to update service");
+			return error;
+		}
+
+		try {
+			console.log("Service task definition changed to: ",await gettaskdef(service,cluster));
+		} catch (error){
+			console.log("Unable to get task definition");
+			return error;
+		}
+	}
+
+	function gettaskdef(service, cluster){
+		return new Promise(function(resolve, reject){
+		var sparams = {
+  		services: [ service ],
+  		cluster: cluster
+		};
+		ecs.describeServices(sparams, function(err, data) {
+	   		if (err) reject(err);
+	   		else resolve(data.services[0].taskDefinition);
+	   	});
+	})}
+
+	function listtaskdef(taskfamily){
+		return new Promise(function(resolve, reject){
+		var params = {
+			familyPrefix: taskfamily,
+			maxResults: 5,
+			sort: "DESC",
+			status: "ACTIVE"
+		};
+		ecs.listTaskDefinitions(params, function(err, data){
+			if (err) reject(err);
+		   	else resolve(data.taskDefinitionArns[1]);
+		});
+	})}
+
+	function updateservice(service,cluster,taskDefinition){
+		return new Promise(function(resolve, reject){
+		var params = {
+		   	service: service,
+		   	cluster: cluster,
+		   	taskDefinition: taskDefinition
+		};
+		ecs.updateService(params, function(err, data) {
+		   if (err) reject(err);
+		   else resolve(data.service.events[0]);
+		});
+	})}
+};
